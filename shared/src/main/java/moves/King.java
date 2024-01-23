c61d094498e3abc36990ede984bff76ae0f681ea
Yu Chen <cy928@byu.edu>
2024-01-23 01:07:14 -0700

    King move

--- shared/src/main/java/moves/King.java
+++ shared/src/main/java/moves/King.java
@@ -1,2 +1,39 @@
-package moves;public class King {
+package moves;
+import chess.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+public class King extends ChessPiece {
+  public King(ChessGame.TeamColor pieceColor) {
+    super(pieceColor, PieceType.BISHOP);
+  }
+  public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor color) {
+    List<ChessMove> validMoves = new ArrayList<>();
+
+    int[][] directions = {
+            { -1, -1 }, { -1, 0 }, { -1, 1 },
+            { 0, -1 },             { 0, 1 },
+            { 1, -1 }, { 1, 0 },  { 1, 1 }
+    };
+
+    for (int[] direction : directions) {
+      int newRow = myPosition.getRow() + direction[0];
+      int newCol = myPosition.getColumn() + direction[1];
+
+      if (isValidMove(newRow, newCol)) {
+        ChessPosition newPosition = new ChessPosition(newRow, newCol);
+        ChessPiece targetPiece = board.getPiece(newPosition);
+
+        if (targetPiece == null || targetPiece.getTeamColor() != getTeamColor()) {
+          validMoves.add(new ChessMove(myPosition, newPosition, null));
+        }
+      }
+    }
+
+    return validMoves;
+  }
+
+  private boolean isValidMove(int row, int col) {
+    return row >= 1 && row <= 8 && col >= 1 && col <= 8;
+  }
 }

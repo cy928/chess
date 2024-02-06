50e17ecfe78f6a73f1fb5987f728f0b3793a56e2
Yu Chen <cy928@byu.edu>
2024-02-06 01:49:57 -0700

    isInCheckmate

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -123,7 +123,7 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-      
+        return isInCheck(teamColor) && isInStalemate(teamColor);
     }
 
     /**
